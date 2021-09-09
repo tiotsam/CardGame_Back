19ac9e60f655a3dd420564f7406f2bb97997d6be
DevLinky <86600420+DevLinky@users.noreply.github.com>
2021-09-09 17:11:34 +0200

    Update ListeDeckModel.js

---
+++ models/ListeDeckModel.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+// mongoose.Promise = global.Promise;
+
+const DecklistModel = mongoose.model('Decklist', new Schema({
+    userId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Users',
+        required: true
+    },
+    deckId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Decks'
+    }
+}));
+module.exports = {DecklistModel};
